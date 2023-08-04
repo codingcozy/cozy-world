7e29e629235d6e2eed6405511f4a0cfb632338a6
cozy-coding <codingcozy@gmail.com>
2023-08-04 18:01:11 +0900

    modify sitemap.xml

--- scripts/sitemap.js
+++ scripts/sitemap.js
@@ -43,7 +43,13 @@ const pagesSitemapGenerator = async () => {
           return `
             <url>
               <loc>${YOUR_AWESOME_DOMAIN}/${routePath}</loc>
-              ${langList.map((lang) => `<xhtml:link rel="alternate" hreflang="${lang}" href="${YOUR_AWESOME_DOMAIN}/${langpath.replace("[lang]", lang)}" />`).join("\n")}
+              ${langList
+                .map((lang2) => {
+                  if (lang2 !== lang) {
+                    return `<xhtml:link rel="alternate" hreflang="${lang}" href="${YOUR_AWESOME_DOMAIN}/${langpath.replace("[lang]", lang2)}" />`;
+                  }
+                })
+                .join("\n")}
               <lastmod>${getDate}</lastmod>
             </url>
           `;
@@ -64,7 +70,13 @@ const categoriesSitemapGenerator = () => {
           (category) =>
             `<url>
               <loc>${YOUR_AWESOME_DOMAIN}/${lang}/posts/${category}</loc>
-              ${langList.map((lang2) => `<xhtml:link rel="alternate" hreflang="${lang2}" href="${YOUR_AWESOME_DOMAIN}/${lang2}/posts/${category}" />`).join("\n")}
+              ${langList
+                .map((lang2) => {
+                  if (lang2 === lang) {
+                    return `<xhtml:link rel="alternate" hreflang="${lang2}" href="${YOUR_AWESOME_DOMAIN}/${lang2}/posts/${category}" />`;
+                  }
+                })
+                .join("\n")}
               <lastmod>${getDate}</lastmod>
             </url>
             `
@@ -102,7 +114,13 @@ const postsSitemapGenerator = async () => {
             return `
             <url>
               <loc>${YOUR_AWESOME_DOMAIN}/${lang}/${routePath}</loc>
-              ${langList.map((lang2) => `<xhtml:link rel="alternate" hreflang="${lang2}" href="${YOUR_AWESOME_DOMAIN}/${lang2}/${routePath}" />`).join("\n")}
+              ${langList
+                .map((lang2) => {
+                  if (lang2 !== lang) {
+                    return `<xhtml:link rel="alternate" hreflang="${lang2}" href="${YOUR_AWESOME_DOMAIN}/${lang2}/${routePath}" />`;
+                  }
+                })
+                .join("\n")}
               <lastmod>${getDate}</lastmod>
             </url>
           `;
@@ -119,11 +137,11 @@ const postsSitemapGenerator = async () => {
   console.log(await pagesSitemapGenerator());
   const generatedSitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
-    <urlset
-      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"
-      xmlns:xhtml="http://www.w3.org/1999/xhtml"
+    <urlset 
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd http://www.w3.org/1999/xhtml http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd"
+      xmlns:xhtml="http://www.w3.org/1999/xhtml" 
     >
       ${await pagesSitemapGenerator()}
       ${categoriesSitemapGenerator()}
